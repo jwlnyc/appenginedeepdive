1efc207415064d90308adbc4d2d0795728b6facf
Joe-at-LA <36517206+Joe-at-LA@users.noreply.github.com>
2018-02-20 16:44:51 -0600

    Update crud.py

--- app-engine/bookshelf/crud.py
+++ app-engine/bookshelf/crud.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from bookshelf import get_model
+from gallery import get_model
 from flask import Blueprint, redirect, render_template, request, url_for
 
 
@@ -23,19 +23,19 @@ crud = Blueprint('crud', __name__)
 @crud.route("/")
 def list():
     token = request.args.get('page_token', None)
-    books, next_page_token = get_model().list(cursor=token)
+    albums, next_page_token = get_model().list(cursor=token)
 
     return render_template(
         "list.html",
-        books=books,
+        albums=albums,
         next_page_token=next_page_token)
 # [END list]
 
 
 @crud.route('/<id>')
 def view(id):
-    book = get_model().read(id)
-    return render_template("view.html", book=book)
+    album = get_model().read(id)
+    return render_template("view.html", album=album)
 
 
 # [START add]
@@ -44,26 +44,26 @@ def add():
     if request.method == 'POST':
         data = request.form.to_dict(flat=True)
 
-        book = get_model().create(data)
+        album = get_model().create(data)
 
-        return redirect(url_for('.view', id=book['id']))
+        return redirect(url_for('.view', id=album['id']))
 
-    return render_template("form.html", action="Add", book={})
+    return render_template("form.html", action="Add", album={})
 # [END add]
 
 
 @crud.route('/<id>/edit', methods=['GET', 'POST'])
 def edit(id):
-    book = get_model().read(id)
+    album = get_model().read(id)
 
     if request.method == 'POST':
         data = request.form.to_dict(flat=True)
 
-        book = get_model().update(data, id)
+        album = get_model().update(data, id)
 
-        return redirect(url_for('.view', id=book['id']))
+        return redirect(url_for('.view', id=album['id']))
 
-    return render_template("form.html", action="Edit", book=book)
+    return render_template("form.html", action="Edit", album=album)
 
 
 @crud.route('/<id>/delete')
